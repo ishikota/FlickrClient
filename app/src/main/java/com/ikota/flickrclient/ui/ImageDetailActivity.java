e5f9135c47fd779ede9216ecc610e10f0b064512
Kota Ishimoto <ishikota086@gmail.com>
2015-08-14 08:07:27 +0900

    Put menu icons on detail screen

--- app/src/main/java/com/ikota/flickrclient/ui/ImageDetailActivity.java
+++ app/src/main/java/com/ikota/flickrclient/ui/ImageDetailActivity.java
@@ -1,6 +1,8 @@
 package com.ikota.flickrclient.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ikota.flickrclient.R;
 
@@ -15,8 +17,9 @@ public class ImageDetailActivity extends BaseActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_detail);
-
-
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
 
         if (savedInstanceState == null) {
             String json = getIntent().getStringExtra(EXTRA_CONTENT);
@@ -24,6 +27,27 @@ public class ImageDetailActivity extends BaseActivity{
                     .add(R.id.container, ImageDetailFragment.newInstance(json))
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            supportFinishAfterTransition();
+        } else if(id == R.id.action_download){
+            return true;
+        } else if(id == R.id.action_share) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
