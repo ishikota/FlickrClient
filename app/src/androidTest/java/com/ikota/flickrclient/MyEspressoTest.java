977c65a46e427f3c93d8d59e11a874c2e57a4ebd
Kota Ishimoto <ishikota086@gmail.com>
2015-08-17 16:54:23 +0900

    Changed to use IdlingResource  to wait loading of list item

--- app/src/androidTest/java/com/ikota/flickrclient/MyEspressoTest.java
+++ app/src/androidTest/java/com/ikota/flickrclient/MyEspressoTest.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
@@ -51,28 +52,57 @@ public class MyEspressoTest extends ActivityInstrumentationTestCase2<MainActivit
         assertNotNull("recyclerView is null", recyclerView);
     }
 
-    public void testRecyclerView_click_after_load() {
-        // TODO : Fix to wait until list item is loaded.(wait 3 seconds may cause error.)
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertNotNull("adapter is null", recyclerView.getAdapter());
-        assertTrue("list item is empty", recyclerView.getAdapter().getItemCount() != 0);
-
+    public void testRecyclerView_click() {
         // Set up an ActivityMonitor
         Instrumentation.ActivityMonitor receiverActivityMonitor =
                 getInstrumentation().addMonitor(ImageDetailActivity.class.getName(),null, false);
 
-        // start DetailActivity by clicking list item.
+        // wait until list item is loaded
+        IdlingResource idlingResource = new ListLoadIdlingResource(recyclerView);
+        Espresso.registerIdlingResources(idlingResource);
+
+        // start DetailActivity by clicking RecyclerView item.
         Espresso.onView(ViewMatchers.withId(android.R.id.list)).perform(
                 RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
 
-        // do assertion for DetailActivity
+        // unregister idling resources
+        Espresso.unregisterIdlingResources(idlingResource);
+
+        // assertion to DetailActivity
         Activity activity = receiverActivityMonitor.waitForActivityWithTimeout(1000);
         assertNotNull("DetailActivity is not null", activity);
         assertEquals("Launched Activity is not ImageDetailActivity", ImageDetailActivity.class, activity.getClass());
+    }
+
+    private class ListLoadIdlingResource implements IdlingResource {
+
+        private ResourceCallback resourceCallback;
+        private RecyclerView recyclerView;
+
+        private ListLoadIdlingResource(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public String getName() {
+            return ListLoadIdlingResource.class.getSimpleName();
+        }
+
+        @Override
+        public boolean isIdleNow() {
+            // check if recyclerView is set loaded items
+            boolean idle = recyclerView.getAdapter()!=null &&
+                    recyclerView.getAdapter().getItemCount() != 0;
+            if (idle && resourceCallback != null) {
+                resourceCallback.onTransitionToIdle();
+            }
+            return idle;
+        }
+
+        @Override
+        public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+            this.resourceCallback = resourceCallback;
+        }
     }
 
 }
