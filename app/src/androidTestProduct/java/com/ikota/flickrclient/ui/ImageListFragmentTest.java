d62f18d4c8c6849a03dc362b666de47a8576ffd9
Kota Ishimoto <ishikota086@gmail.com>
2015-08-24 14:20:36 +0900

    Added test to check column num of list in different orientation.

--- app/src/androidTestProduct/java/com/ikota/flickrclient/ui/ImageListFragmentTest.java
+++ app/src/androidTestProduct/java/com/ikota/flickrclient/ui/ImageListFragmentTest.java
@@ -2,6 +2,8 @@ package com.ikota.flickrclient.ui;
 
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
@@ -9,15 +11,17 @@ import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.ikota.flickrclient.IdlingResource.ListCountIdlingResource;
 import com.ikota.flickrclient.IdlingResource.LoadingIdlingResource;
-import com.ikota.flickrclient.R;
 import com.ikota.flickrclient.IdlingResource.TimingIdlingResource;
+import com.ikota.flickrclient.R;
 import com.ikota.flickrclient.data.DataHolder;
 import com.ikota.flickrclient.di.DummyAPIModule;
+import com.ikota.flickrclient.di.PopularListModule;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -31,6 +35,7 @@ import dagger.ObjectGraph;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -121,6 +126,55 @@ public class ImageListFragmentTest extends ActivityInstrumentationTestCase2<Popu
         Espresso.registerIdlingResources(idlingResource_72);
         onView(withId(android.R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, scrollTo()));
         assertEquals(72, recyclerView.getAdapter().getItemCount());
+    }
+
+    /**
+     * TODO: This test doesn't check if grid cell size is set correctly.
+     * Reference URL of how to change configuration.
+     * https://gist.github.com/Skipants/1001366/874b8d4b51bcd41ea95de408b9558e3cc557fe4f
+     */
+    @Test
+    public void checkColumnNumInDifferntOrientation() {
+        PopularListActivity activity = activityRule.launchActivity(new Intent());
+        ImageListFragment fragment = (ImageListFragment)activity.getSupportFragmentManager()
+                .findFragmentByTag(ImageListFragment.class.getSimpleName());
+        @SuppressWarnings("ConstantConditions")
+        RecyclerView recyclerView = (RecyclerView)fragment.getView().findViewById(android.R.id.list);
+
+        // assertion in  portrait mode
+        ListCountIdlingResource idlingResource1 = new ListCountIdlingResource(recyclerView, 1);
+        Espresso.registerIdlingResources(idlingResource1);
+        onView(withId(android.R.id.list)).perform(swipeLeft());
+        Espresso.unregisterIdlingResources(idlingResource1);
+        assertEquals(PopularListModule.PORTRAIT_COL,
+                ((GridLayoutManager) recyclerView.getLayoutManager()).getSpanCount());
+        activity.finish();
+
+        // change orientation to landscape
+        Resources instrumentResources = InstrumentationRegistry.getInstrumentation().getTargetContext().getResources();
+        Configuration originalConfig = instrumentResources.getConfiguration();
+        Configuration changeableConfig = new Configuration(originalConfig);
+        changeableConfig.orientation = Configuration.ORIENTATION_LANDSCAPE; // Phone switches to landscape
+        instrumentResources.updateConfiguration(changeableConfig, instrumentResources.getDisplayMetrics()); // Now the phone is "in landscape"
+
+        // assertion in landscape mode
+        PopularListActivity activity2 = activityRule.launchActivity(new Intent());
+        ImageListFragment fragment2 = (ImageListFragment)activity2.getSupportFragmentManager()
+                .findFragmentByTag(ImageListFragment.class.getSimpleName());
+        @SuppressWarnings("ConstantConditions")
+        RecyclerView recyclerView2 = (RecyclerView)fragment2.getView().findViewById(android.R.id.list);
+        ListCountIdlingResource idlingResource2 = new ListCountIdlingResource(recyclerView, 1);
+        Espresso.registerIdlingResources(idlingResource2);
+        onView(withId(android.R.id.list)).perform(swipeLeft());
+        Espresso.unregisterIdlingResources(idlingResource2);
+        assertEquals(PopularListModule.HORIZONTAL_COL,
+                ((GridLayoutManager) recyclerView2.getLayoutManager()).getSpanCount());
+        activity.finish();
+
+        // return the orientation to portrait mode
+        changeableConfig.orientation = Configuration.ORIENTATION_PORTRAIT; // Phone switches to landscape
+        instrumentResources.updateConfiguration(changeableConfig, instrumentResources.getDisplayMetrics()); // Now the phone is "in landscape"
+
     }
 
 }
