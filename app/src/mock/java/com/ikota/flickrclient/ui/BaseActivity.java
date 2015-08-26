4287f3971bec19d0441c00f346c5bf17614d203b
Kota Ishimoto <ishikota086@gmail.com>
2015-08-26 16:40:57 +0900

    Changed UserPage structure to support Tab switch. Moved shared elements to host activity like Tab, cover, ..

--- app/src/mock/java/com/ikota/flickrclient/ui/BaseActivity.java
+++ app/src/mock/java/com/ikota/flickrclient/ui/BaseActivity.java
@@ -3,6 +3,7 @@ package com.ikota.flickrclient.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ikota.flickrclient.R;
 
@@ -19,6 +20,17 @@ public class BaseActivity extends AppCompatActivity{
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
         getActionBarToolbar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            supportFinishAfterTransition();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     protected Toolbar getActionBarToolbar() {
--- app/src/mock/java/com/ikota/flickrclient/ui/UserActivity.java
+++ app/src/mock/java/com/ikota/flickrclient/ui/UserActivity.java
@@ -1,44 +1,121 @@
 package com.ikota.flickrclient.ui;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
-import android.view.MenuItem;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 import com.ikota.flickrclient.R;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
 
 public class UserActivity extends BaseActivity{
+
+    static Bus sTabEventBus = new Bus();
+
+    private UserBaseFragment mFragment;
+    private TabLayout mTabLayout;
+
+    private int mActionBarHeight;
+    private int mTabHeight;
+    private int mHeaderPadding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //noinspection ConstantConditions
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
+        setContentView(R.layout.activity_user);
+        setupActionBar();
+        setupViews();
+        getViewSize();
 
         FragmentManager fm = getSupportFragmentManager();
-        UserFragment fragment = (UserFragment) fm.findFragmentByTag(UserFragment.class.getSimpleName());
-        if (fragment == null) {
-            fragment = new UserFragment();
+        mFragment = (UserBaseFragment)
+                fm.findFragmentByTag(UserBaseFragment.class.getSimpleName());
+        if (mFragment == null) {
+            mFragment = new UserBaseFragment();
             String tag = UserFragment.class.getSimpleName();
-            fm.beginTransaction()
-                    .add(R.id.container, fragment, tag)
-                    .commit();
+            fm.beginTransaction().add(R.id.container, mFragment, tag).commit();
         }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    public void onResume() {
+        super.onResume();
+        sTabEventBus.register(this);
+    }
 
-        if (id == android.R.id.home) {
-            supportFinishAfterTransition();
+    @Override
+    public void onStop() {
+        super.onStop();
+        sTabEventBus.unregister(this);
+    }
+
+    private void getViewSize() {
+        mTabHeight = (int)getResources().getDimension(R.dimen.user_tab_height);
+        mHeaderPadding = (int)getResources().getDimension(R.dimen.user_header_padding);
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)){
+            mActionBarHeight = TypedValue.
+                    complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+    }
+
+    private void setupActionBar() {
+        //noinspection ConstantConditions
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
+    }
+
+    private void setupViews() {
+        Resources r = getResources();
+        ((TextView)findViewById(R.id.user_name)).setText(r.getString(R.string.username));
+        ((TextView) findViewById(R.id.sub_text)).setText(r.getString(R.string.user_info));
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mTabLayout.addTab(mTabLayout.newTab().setText("ABOUTS"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("POST"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("PHOTOS"));
+
+        if(mActionBarToolbar!=null)  // nullpo occurred when config changes
+            mActionBarToolbar.getBackground().setAlpha(0);
+    }
+
+    @Subscribe
+    public void receiveGridViewEvent(ScrollEvent event) {
+        // Log.i("EventBus", "TabEvent Received (list_top:" + event.list_top + ",dy:" + event.dy + ")");
+
+        // scroll TabLayout with RecyclerView scroll
+        float y = mTabLayout.getY();
+        if (y == 0) return;  // ignore setup phase callback
+        if (event.dy >= 0) {  // if up scroll
+            mTabLayout.setY(Math.max(mActionBarHeight, event.list_top-mTabHeight));    // tab stops below actionbar
+        } else {    // if down scroll
+            if(event.list_top >= y+mTabHeight) {  // do not down TabLayout until list head is revealed
+                mTabLayout.setY(Math.min(event.list_top-mTabHeight, mHeaderPadding));
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        // change actionbar alpha with RecyclerView scroll
+        if(mActionBarToolbar!=null) {  // nullpo occured when config changes
+            int alpha = (int) (255 *
+                    (1 - (y - mActionBarHeight) / (mHeaderPadding - mTabHeight - mActionBarHeight)));
+            mActionBarToolbar.getBackground().setAlpha(alpha);
+        }
     }
+
+    static class ScrollEvent {
+        final int list_top;
+        final int dy;
+        public ScrollEvent(int list_top, int dy) {
+            this.list_top = list_top;
+            this.dy = dy;
+        }
+    }
+
 
 }
