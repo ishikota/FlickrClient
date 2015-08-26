739abb16daa3db9873d9f805e4ac8ef42d1a219b
Kota Ishimoto <ishikota086@gmail.com>
2015-08-26 22:06:41 +0900

    Added home click handling in BaseActivity

--- app/src/product/java/com/ikota/flickrclient/ui/BaseActivity.java
+++ app/src/product/java/com/ikota/flickrclient/ui/BaseActivity.java
@@ -3,6 +3,7 @@ package com.ikota.flickrclient.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ikota.flickrclient.R;
 
@@ -32,6 +33,17 @@ public class BaseActivity extends AppCompatActivity{
     public void onBackPressed() {
         super.onBackPressed();
         supportFinishAfterTransition();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            supportFinishAfterTransition();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     protected Toolbar getActionBarToolbar() {
