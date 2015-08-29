8a4510d3972cb0ed86dd7e37cadfa7907fd20d31
Kota Ishimoto <ishikota086@gmail.com>
2015-08-29 13:12:57 +0900

    Updated DetailFragmentTest to use mock server

--- app/src/androidTestProduct/java/com/ikota/flickrclient/ui/ImageDetailFragmentTest.java
+++ app/src/androidTestProduct/java/com/ikota/flickrclient/ui/ImageDetailFragmentTest.java
@@ -14,11 +14,19 @@ import android.widget.TextView;
 import com.ikota.flickrclient.R;
 import com.ikota.flickrclient.IdlingResource.TextIdlingResource;
 import com.ikota.flickrclient.data.DataHolder;
+import com.ikota.flickrclient.di.DummyAPIModule;
+import com.ikota.flickrclient.network.Util;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import dagger.ObjectGraph;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -28,7 +36,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class ImageDetailFragmentTest extends ActivityInstrumentationTestCase2<ImageDetailActivity> {
 
-    private Context context;
     private Intent intent;
 
     public ImageDetailFragmentTest() {
@@ -41,13 +48,34 @@ public class ImageDetailFragmentTest extends ActivityInstrumentationTestCase2<Im
             true,     // initialTouchMode
             false);   // launchActivity. False so we can customize the intent per test method
 
+    private void setupMockServer(HashMap<String, String> override_map) {
+        HashMap<String, String> map = Util.RESPONSE_MAP;
+
+        if(override_map!=null) {
+            for (String key : override_map.keySet()) {
+                map.put(key, override_map.get(key));
+            }
+        }
+
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        AndroidApplication app =
+                (AndroidApplication) instrumentation.getTargetContext().getApplicationContext();
+
+        // setup objectGraph to inject Mock API
+        List modules = Collections.singletonList(new DummyAPIModule(map));
+        ObjectGraph graph = ObjectGraph.create(modules.toArray());
+        app.setObjectGraph(graph);
+        app.getObjectGraph().inject(app);
+    }
+
     @Before
     public void setUp() throws Exception {
         super.setUp();
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-        context = instrumentation.getTargetContext();
+        Context context = instrumentation.getTargetContext();
         intent = new Intent(context, ImageDetailActivity.class);
         intent.putExtra(ImageDetailActivity.EXTRA_CONTENT, DataHolder.LIST_ITEM_JSON);
+        intent.putExtra(ImageDetailActivity.EXTRA_CACHE_SIZE, "");
     }
 
     @Test
@@ -58,13 +86,14 @@ public class ImageDetailFragmentTest extends ActivityInstrumentationTestCase2<Im
         assertEquals(args, DataHolder.LIST_ITEM_JSON);
     }
 
-
-
+    /** TODO: Passed List data and detail data is wrong item */
     @Test
     public void checkDetailInfo_isSet() {
+        setupMockServer(null);
         ImageDetailActivity activity = activityRule.launchActivity(intent);
         ImageDetailFragment fragment = (ImageDetailFragment)activity.getSupportFragmentManager().findFragmentByTag(
                 ImageDetailFragment.class.getSimpleName());
+        //noinspection ConstantConditions
         TextView use_name = (TextView)fragment.getView().findViewById(R.id.user_name);
         onView(withId(R.id.title)).check(matches(withText("A Quiet Evening")));  // we know its title from list
         onView(withId(R.id.user_name)).check(matches(withText("")));
@@ -73,9 +102,9 @@ public class ImageDetailFragmentTest extends ActivityInstrumentationTestCase2<Im
         TextIdlingResource idlingResource = new TextIdlingResource(use_name);
         Espresso.registerIdlingResources(idlingResource);
         onView(withId(R.id.title)).check(matches(withText("A Quiet Evening")));
-        onView(withId(R.id.user_name)).check(matches(withText("Jens Haggren")));
-        //onView(withId(R.id.description)).check(matches(withText("Thank you very much for your views, faves and comments. I appreciate it a lot!")));
-        onView(withId(R.id.date_text)).check(matches(withText("2015-06-21 21:18:57")));
+        onView(withId(R.id.user_name)).check(matches(withText("Cole Chase Photography")));
+        onView(withId(R.id.description)).check(matches(withText("I was treated to this brilliant sunset on my recent trip to Oregon.  This photo was captured in Ecola State Park which is one of my favorite places to visit on the coast. The deactivated Tillamook Rock Lighthouse is seen on the small island in the distance.  Comments &amp; views are always appreciated. Thanks for looking &amp; have a wonderful Sunday!")));
+        onView(withId(R.id.date_text)).check(matches(withText("2015-08-02 22:40:28")));
     }
 
 }
